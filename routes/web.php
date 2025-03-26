90507479bc221d7b4bbf86b9941bc33deca36e1f
Afrees <contactoomunoz@gmail.com>
2025-03-26 15:03:48 -0500

    Creacion de vistas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CorpsController;
+use App\Http\Controllers\BarracksController;
+use App\Http\Controllers\CompaniesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resourse('corps', CorpsController::class);
+Route::resourse('barracks', BarracksController::class);
+Route::resourse('companies', CompaniesController::class);
