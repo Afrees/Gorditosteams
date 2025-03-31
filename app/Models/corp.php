d94ef4cab36b712edf4ca4722e4ec05eb53a2308
Afrees <contactoomunoz@gmail.com>
2025-03-31 17:19:32 -0500

    corp
    sd

--- app/Models/corp.php
+++ app/Models/corp.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Corp extends Model {
-    use HasFactory;
-    protected $fillable = ['denomination'];
+  //
 }
